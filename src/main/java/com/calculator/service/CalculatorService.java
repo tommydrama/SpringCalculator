aaa847d2e5cb4ef9cdf5c22257f1bb2d62382c1c
tommydrama <tomek1darowski@gmail.com>
2018-11-12 18:56:08 +0100

    add option clear

--- src/main/java/com/calculator/service/CalculatorService.java
+++ src/main/java/com/calculator/service/CalculatorService.java
@@ -1,4 +1,35 @@
 package com.calculator.service;
 
+
+import com.calculator.model.Calculator;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculatorService {
+
+    public double add(Calculator calculator){
+        return calculator.getA()+calculator.getB();
+    }
+
+    public double subtract(Calculator calculator){
+        return calculator.getA()- calculator.getB();
+    }
+
+    public double multiply(Calculator calculator){
+        return calculator.getA()*calculator.getB();
+    }
+
+    public double divide(Calculator calculator){
+        if(calculator.getA()==0 || calculator.getB()==0){
+            return 0;
+        }
+        return calculator.getA()/calculator.getB();
+    }
+
+    public Calculator clear(Calculator calculator){
+        calculator.setA(0);
+        calculator.setB(0);
+        calculator.setSingleOpp(0);
+        return calculator;
+    }
 }
